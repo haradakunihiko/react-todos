37a15418685aa29e2ecca3e7ee7aabf8971bbb30
harada <sthsoulful@gmail.com>
2015-07-08 22:48:20 +0900

    create basic html
    Conflicts:
    	todomvc_com/src/js/app.js

--- todomvc_com/src/js/app.js
+++ todomvc_com/src/js/app.js
@@ -1,8 +1,42 @@
-var React = require('react/addons');
-var util = require('./util.js');
+(function(){
+    var React = require('react/addons');
+    //var util = require('./util.js');
 
-React.render(
-    <h1>TODOs</h1>,
-    document.getElementById('todo-app')
-);
-util();
+    var TodoApp = React.createClass({
+        render: function(){
+            return (
+                <div>
+                    <header>
+                        <h1>TODO</h1>
+                    </header>
+                    <section>
+                        <button type="button"></button>
+                        <input type="text"></input>
+                    </section>
+                    <section>
+                        <ul>
+                            <li>
+                                <input type="checkbox"></input>
+                                <span></span>
+                                <button type="button"></button>
+                            </li>
+                        </ul>
+                    </section>
+                    <footer>
+                        <span>1 item left</span>
+                        <button type="button">All</button>
+                        <button type="button">Active</button>
+                        <button type="button">Completed</button>
+                    </footer>
+                </div>
+            );
+        }
+    });
+
+    React.render(
+        <TodoApp></TodoApp>,
+        document.getElementById('todo-app')
+    );
+})();
+
+//util();
