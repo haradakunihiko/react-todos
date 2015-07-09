844627e5abd6aaba5d673693393f1c5924b46da1
harada <sthsoulful@gmail.com>
2015-07-09 11:41:12 +0900

    add css
    Conflicts:
    	todomvc_com/src/js/app.js

--- todomvc_com/src/js/app.js
+++ todomvc_com/src/js/app.js
@@ -5,31 +5,63 @@
     var TodoApp = React.createClass({
         render: function(){
             return (
-                <div >
+                <div className="container">
                     <header>
-                        <h1>TODO</h1>
+                        <h1>todos</h1>
                     </header>
-                    <section>
-                        <input  type="text"></input>
-                    </section>
-                    <section>
-                        <button type="button"></button>
-                        <ul>
-                            <li>
-                                <div>
-                                    <input type="checkbox"></input>
-                                    <span></span>
-                                    <button type="button"></button>
+                    <section className="main-area">
+                        <div className="todo-input-area">
+                            <input className="check-all-todos" type="checkbox" ></input>
+                            <input className="todo-input" type="text" placeholder="What needs to be done?"></input>
+                        </div>
+                        <ul className="todo-list">
+                            <li className="todo-list-item">
+                                <div className="todo-list-item-view-box">
+                                    <input className="todo-list-item-check" type="checkbox"></input>
+                                    <span className="todo-list-item-label">aiueo</span>
+                                    <button className="todo-list-item-remove" type="button"></button>
+                                </div>
+                                <div className="todo-list-item-edit-box">
+                                    <input type="text"></input>
+                                </div>
+                            </li>
+                            <li className="todo-list-item completed">
+                                <div className="todo-list-item-view-box">
+                                    <input className="todo-list-item-check" type="checkbox"></input>
+                                    <span className="todo-list-item-label">kakiku</span>
+                                    <button className="todo-list-item-remove" type="button"></button>
+                                </div>
+                                <div className="todo-list-item-edit-box">
+                                    <input type="text"></input>
+                                </div>
+                            </li>
+                            <li className="todo-list-item editing">
+                                <div className="todo-list-item-view-box">
+                                    <input className="todo-list-item-check" type="checkbox"></input>
+                                    <span className="todo-list-item-label">kakiku</span>
+                                    <button className="todo-list-item-remove" type="button"></button>
+                                </div>
+                                <div className="todo-list-item-edit-box">
+                                    <input type="text"></input>
                                 </div>
                             </li>
                         </ul>
+                        <footer>
+                            <span>1 item left</span>
+                            <ul className="filter-list">
+                                <li className="filter-list-item">
+                                    <a href="#all" className="selected">All</a>
+                                </li>
+                                <li className="filter-list-item">
+                                    <a href="#active" >Active</a>
+                                </li>
+                                <li className="filter-list-item">
+                                    <a href="#completed" >Completed</a>
+                                </li>
+                            </ul>
+                            <button type="button" className="clear-completed">clear completed</button>
+                        </footer>
                     </section>
-                    <footer>
-                        <span>1 item left</span>
-                        <button type="button">All</button>
-                        <button type="button">Active</button>
-                        <button type="button">Completed</button>
-                    </footer>
                 </div>
             );
         }
